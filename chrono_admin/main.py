f1a5718d92646eb57ecf434ce535cebeac2c952f
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2026-01-22 18:07:30 +0100

    Create main.py

---
+++ chrono_admin/main.py
@@ -0,0 +1,14 @@
+"""
+CHRONO Admin aplikace – spuštění
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from login import LoginWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
